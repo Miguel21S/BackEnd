a3e1648f89976fbc024d7e5401ed6924a0f9db8d
Miguel21S <leaismiguelss@gamail.com>
2024-02-27 10:26:50 +0100

    Subir la alteración de la tabla user

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,6 +4,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Roles1708946185209 } from "./migrations/1708946185209-roles";
 import { Users1708948581768 } from './migrations/1708948581768-users';
+import { Users1709025174650 } from './migrations/1709025174650-users';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -13,7 +14,7 @@ export const AppDataSource = new DataSource({
     password: "2345",
     database: "new_libreria",
     entities: [],
-    migrations:[Roles1708946185209, Users1708948581768],
+    migrations:[Roles1708946185209, Users1708948581768, Users1709025174650],
     synchronize: false,
     logging: false,
 })
